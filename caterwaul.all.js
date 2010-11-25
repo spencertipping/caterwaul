f4db269e8d7c170d5e865c58016b2d4ae501007f
Spencer Tipping <spencer@spencertipping.com>
2010-11-25 15:15:24 -0700

    Rewrote the sequence library (for infinite sequences so far), and added a centralized magic() method to caterwaul. Also added /re[] notation.

--- caterwaul.all.js
+++ caterwaul.all.js
@@ -933,6 +933,12 @@ parse_associates_right = hash('= += -= *= /= %= &= ^= |= <<= >>= >>>= ~ ! new ty
 
     shallow('util', {extend: extend, merge: merge, se: se, macro_try_match: macro_try_match, id: id, bind: bind, map: map, qw: qw}).
 
+// Magic.
+// Sometimes you need to grab a unique value that is unlikely to exist elsewhere. Caterwaul gives you such a value given a string. These values are shared across all Caterwaul instances and are
+// considered to be opaque. Because of the possibility of namespace collisions, you should name your magic after a configuration or otherwise prefix it somehow.
+
+     method('magic', (function (table) {return function (name) {return table[name] || (table[name] = {})}})({})).
+
 // Standard library.
 // Caterwaul ships with a standard library of useful macros, though they aren't activated by default. To activate them, you say something like this:
 
@@ -1000,10 +1006,13 @@ parse_associates_right = hash('= += -= *= /= %= &= ^= |= <<= >>= >>>= ~ ! new ty
 
 //   Side-effects can be chained since / is left-associative.
 
+//   An alternative form of side-effecting is the 'right-handed' side-effect, written x /re[y]. This returns the result of evaluating y, where _ is bound to x.
+
     tconfiguration('std.qs std.qg std.fn', 'std.obj', function () {
       this.configure('std.qg std.fn').rmacro(qs[_/mb/_],  fn[object, method][qs[qg[fn[_o][fn_[_o.m.apply (_o, arguments)]]](o)].replace({_o: this.gensym(), o: object, m: method})]).
                                       rmacro(qs[_/mb[_]], fn[object, method][qs[qg[fn[_o][fn_[_o[m].apply(_o, arguments)]]](o)].replace({_o: this.gensym(), o: object, m: method})]).
-                                      rmacro(qs[_/se[_]], fn   [value, body][qs[qg[fn[_][body, _]](value)].replace({body: body, value: value})])}).
+                                      rmacro(qs[_/se[_]], fn   [value, body][qs[qg[fn[_][body, _]].call(this, value)].replace({body: body, value: value})]).
+                                      rmacro(qs[_/re[_]], fn   [value, body][qs[qg[fn[_]   [body]].call(this, value)].replace({body: body, value: value})])}).
 
 //   Binding abbreviations (the 'bind' library).
 //   Includes forms for defining local variables. One is 'let [bindings] in expression', and the other is 'expression, where[bindings]'. For the second, keep in mind that comma is
@@ -1304,7 +1313,7 @@ parse_associates_right = hash('= += -= *= /= %= &= ^= |= <<= >>= >>>= ~ ! new ty
 // | caterwaul.continuation.call_tail(f, arg1, arg2, ...);
 
   tconfiguration('std', 'continuation.delimited', function () {
-    var magic = this.configure('continuation.core').continuation.magic = {};
+    var magic = this.configure('continuation.core').continuation.magic = this.magic('continuation.delimited');
     this.continuation.call_cc = function (f) {var escaped = false, cc = function (x) {escaped = true; throw x}, frame = {magic: magic, continuation: f, parameters: [cc]};
                                               try       {while ((frame = frame.continuation.apply(this, frame.parameters)) && frame && frame.magic === magic); return frame}
                                               catch (e) {if (escaped) return e; else throw e}};
@@ -1324,4 +1333,64 @@ parse_associates_right = hash('= += -= *= /= %= &= ^= |= <<= >>= >>>= ~ ! new ty
 // Introduction.
 // Javascript's looping facilities are side-effectful and more importantly operate in statement-mode rather than expression-mode. This sequence library moves finite and anamorphic looping into
 // expression-mode using both methods and macros. Macros are used sparingly here; they provide comprehensions, but are ultimately just shorthands for sequence methods.
+
+  caterwaul.tconfiguration('std', 'seq.core', function () {this.shallow('seq', {core: fn_[null] /se[_.prototype = {}]})}).
+
+// There are two kinds of sequences represented here. One is a finite sequence, which is eager and acts like a Javascript array (though it has a different prototype). The other is an infinite
+// stream; this is an anamorphism that generates new elements from previous ones. Because Javascript isn't required to optimize tail calls, any recursion done by the sequence library is coded in
+// CPS using the continuation library.
+
+// Finite sequence API.
+// Finite sequences are assumed to have numbered elements and a 'length' field, just like a Javascript array or jQuery object. Any mapping, filtering, or folding on these sequences is done
+// eagerly (put differently, most sequence/stream operations are closed under eagerness). There's a core prototype for finite sequences that contains eager implementations of each(), map(),
+// filter(), foldl(), foldr(), zip(), etc.
+
+// Stream API.
+// All streams are assumed to be infinite in length; that is, given some element there is always another one. Streams provide this interface with h() and t() methods; the former returns the first
+// element of the stream, and the latter returns a stream containing the rest of the elements.
+
+//   Terminals.
+//   There isn't a 'nil' stream exactly, since that would imply an end of data. Rather, if you're building a stream from consing, you'll end up consing something onto a constant stream. The 'nil'
+//   defined here is actually just a stream that returns undefined forever.
+
+    tconfiguration('std continuation', 'seq.infinite.core', function () {
+      this.configure('seq.core').seq.infinite = fn_[null] /se[_.prototype = new this.seq.core()]
+        /se[_.def(name, ctor, h, t) = i[name] = ctor /se[_.prototype = new i() /se[_.h = h, _.t = t, _.constructor = ctor]], where[i = _],
+
+            _.def('cons', fn[h, t][this._h = h, this._t = t], fn_[this._h], fn_[this._t]),
+            _.def('k',    fn   [x][this._x = x],              fn_[this._x], fn_[this])]}).
+
+//   Anamorphisms via fixed-point.
+//   Anamorphic streams are basically unwrapped version of the Y combinator. An anamorphic stream takes a function f and an initial element x, and returns f(x), f(f(x)), f(f(f(x))), ....
+
+    tconfiguration('std', 'seq.infinite.y', function () {
+      this.configure('seq.infinite.core').seq.infinite.def('y', fn[f, x][this._f = f, this._x = x], fn_[this._x], fn_[new this.constructor(this._f, this._f(this._x))])}).
+
+//   Lazy map and filter.
+//   These are implemented as separate classes that wrap instances of infinite streams. They implement the next() method to provide the desired functionality. map() and filter() are simple
+//   because they provide streams as output.
+
+    tconfiguration('std continuation', 'seq.infinite.class.transform', function () {
+      this.configure('seq.infinite.core').seq.infinite
+        /se[_.prototype.map(f) = new _.map(f, this)] 
+        /se[_.def('map',    fn[f, xs][this._f = f, this._xs = xs], fn_[this._f(this._xs.h())], fn_[new this.constructor(this._f, this._xs.t())])]
+
+        /se[_.prototype.filter(f) = new _.filter(f, this)]
+        /se[_.def('filter', fn[f, xs][this._f = f, this._xs = xs], fn_[let*[f = this._f, next(s)(cc) = let[h = s.h()][f(h) ? cc(h) : call/tail[next(s.t())(cc)]]] in call/cc[next(this)]],
+                                                                   fn_[new this.constructor(this._f, this._xs.t())])]}).
+
+//   Traversal and forcing.
+//   This is where we convert from infinite streams to finite sequences. You can take or drop elements until a condition is met. take() always assumes it will return a finite sequence, whereas
+//   drop() assumes it will return an infinite stream. (In other words, the number of taken or dropped elements is assumed to be finite.) Both take() and drop() are eager.
+
+    tconfiguration('std continuation', 'seq.infinite.class.traversal', function () {
+      this.configure('seq.infinite.core').seq.infinite.prototype
+        /se[_.drop(f) = let*[next(s)(cc) = f(s.h()) ? cc(s.t()) : call/tail[next(s.t())(cc)]] in call/cc[next(this)],
+            _.take(f) = let*[xs = [], next(s)(cc) = let[h = s.h()][f(h) ? cc(xs) : (xs.push(h), call/tail[next(s.t())(cc)])]] in call/cc[next(this)]]}).
+
+// Final configuration.
+// Rather than including individual configurations above, you'll probably just want to include this one.
+
+  configuration('seq', function () {this.configure('seq.core seq.infinite.core seq.infinite.y seq.infinite.class.transform seq.infinite.class.traversal')});
+
 // Generated by SDoc 
--- modules/caterwaul.seq.js
+++ modules/caterwaul.seq.js
@@ -4,4 +4,64 @@
 // Introduction.
 // Javascript's looping facilities are side-effectful and more importantly operate in statement-mode rather than expression-mode. This sequence library moves finite and anamorphic looping into
 // expression-mode using both methods and macros. Macros are used sparingly here; they provide comprehensions, but are ultimately just shorthands for sequence methods.
+
+  caterwaul.tconfiguration('std', 'seq.core', function () {this.shallow('seq', {core: fn_[null] /se[_.prototype = {}]})}).
+
+// There are two kinds of sequences represented here. One is a finite sequence, which is eager and acts like a Javascript array (though it has a different prototype). The other is an infinite
+// stream; this is an anamorphism that generates new elements from previous ones. Because Javascript isn't required to optimize tail calls, any recursion done by the sequence library is coded in
+// CPS using the continuation library.
+
+// Finite sequence API.
+// Finite sequences are assumed to have numbered elements and a 'length' field, just like a Javascript array or jQuery object. Any mapping, filtering, or folding on these sequences is done
+// eagerly (put differently, most sequence/stream operations are closed under eagerness). There's a core prototype for finite sequences that contains eager implementations of each(), map(),
+// filter(), foldl(), foldr(), zip(), etc.
+
+// Stream API.
+// All streams are assumed to be infinite in length; that is, given some element there is always another one. Streams provide this interface with h() and t() methods; the former returns the first
+// element of the stream, and the latter returns a stream containing the rest of the elements.
+
+//   Terminals.
+//   There isn't a 'nil' stream exactly, since that would imply an end of data. Rather, if you're building a stream from consing, you'll end up consing something onto a constant stream. The 'nil'
+//   defined here is actually just a stream that returns undefined forever.
+
+    tconfiguration('std continuation', 'seq.infinite.core', function () {
+      this.configure('seq.core').seq.infinite = fn_[null] /se[_.prototype = new this.seq.core()]
+        /se[_.def(name, ctor, h, t) = i[name] = ctor /se[_.prototype = new i() /se[_.h = h, _.t = t, _.constructor = ctor]], where[i = _],
+
+            _.def('cons', fn[h, t][this._h = h, this._t = t], fn_[this._h], fn_[this._t]),
+            _.def('k',    fn   [x][this._x = x],              fn_[this._x], fn_[this])]}).
+
+//   Anamorphisms via fixed-point.
+//   Anamorphic streams are basically unwrapped version of the Y combinator. An anamorphic stream takes a function f and an initial element x, and returns f(x), f(f(x)), f(f(f(x))), ....
+
+    tconfiguration('std', 'seq.infinite.y', function () {
+      this.configure('seq.infinite.core').seq.infinite.def('y', fn[f, x][this._f = f, this._x = x], fn_[this._x], fn_[new this.constructor(this._f, this._f(this._x))])}).
+
+//   Lazy map and filter.
+//   These are implemented as separate classes that wrap instances of infinite streams. They implement the next() method to provide the desired functionality. map() and filter() are simple
+//   because they provide streams as output.
+
+    tconfiguration('std continuation', 'seq.infinite.class.transform', function () {
+      this.configure('seq.infinite.core').seq.infinite
+        /se[_.prototype.map(f) = new _.map(f, this)] 
+        /se[_.def('map',    fn[f, xs][this._f = f, this._xs = xs], fn_[this._f(this._xs.h())], fn_[new this.constructor(this._f, this._xs.t())])]
+
+        /se[_.prototype.filter(f) = new _.filter(f, this)]
+        /se[_.def('filter', fn[f, xs][this._f = f, this._xs = xs], fn_[let*[f = this._f, next(s)(cc) = let[h = s.h()][f(h) ? cc(h) : call/tail[next(s.t())(cc)]]] in call/cc[next(this)]],
+                                                                   fn_[new this.constructor(this._f, this._xs.t())])]}).
+
+//   Traversal and forcing.
+//   This is where we convert from infinite streams to finite sequences. You can take or drop elements until a condition is met. take() always assumes it will return a finite sequence, whereas
+//   drop() assumes it will return an infinite stream. (In other words, the number of taken or dropped elements is assumed to be finite.) Both take() and drop() are eager.
+
+    tconfiguration('std continuation', 'seq.infinite.class.traversal', function () {
+      this.configure('seq.infinite.core').seq.infinite.prototype
+        /se[_.drop(f) = let*[next(s)(cc) = f(s.h()) ? cc(s.t()) : call/tail[next(s.t())(cc)]] in call/cc[next(this)],
+            _.take(f) = let*[xs = [], next(s)(cc) = let[h = s.h()][f(h) ? cc(xs) : (xs.push(h), call/tail[next(s.t())(cc)])]] in call/cc[next(this)]]}).
+
+// Final configuration.
+// Rather than including individual configurations above, you'll probably just want to include this one.
+
+  configuration('seq', function () {this.configure('seq.core seq.infinite.core seq.infinite.y seq.infinite.class.transform seq.infinite.class.traversal')});
+
 // Generated by SDoc 
