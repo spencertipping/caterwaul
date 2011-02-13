ebb9318a850115b4acd127a9f2c9c45a1b13178d
Spencer Tipping <spencer@spencertipping.com>
2011-02-12 18:29:07 -0700

    Working on the parser. So far the combinator library passes tests, but I know that the times() combinator is broken.

--- modules/caterwaul.parser.js
+++ modules/caterwaul.parser.js
@@ -8,7 +8,7 @@
 //   Basis and acknowledgements.
 //   This parser library is based heavily on Chris Double's JSParse (available at github.com/doublec/jsparse), which implements a memoized combinatory PEG parser. If you are looking for a simple
 //   and well-designed parsing library, I highly recommend JSParse; it will be easier to use and more predictable than caterwaul.parser. Like JSParse, these parsers are memoized and use parsing
-//   expression grammars.
+//   expression grammars. However, this parser is probably quite a lot slower.
 
 // Internals.
 // Memoization is restricted to a session rather than being global. This prevents the space leak that would otherwise occur if the parser outlived the result. Behind the scenes the parser
@@ -17,17 +17,16 @@
 
   caterwaul.tconfiguration('std seq continuation memoize', 'parser.core', function () {
     this.namespace('parser') /se[_.parse_state(input, i, result, memo) = undefined /se[this.input = input, this.i = i, this.result = result, this.memo = memo],
-                                 _.parse_state /se[_.from_input(input) = new _.parse_state(input, 0, null, {}),
-                                                   _.prototype /se[
-                                   _.accept(n, r) = new this.constructor(this.input, this.i + n, r, this.memo),
-                                   _.then(state)  = this.accept(state.i - this.i, this.result instanceof Array ? this.result.concat([state.result]) : [this.result, state.result]),
-                                   _.fail()       = false]],
+                                 _.parse_state /se.s[s.from_input(input)      = new _.parse_state(input, 0, null, {}),
+                                                     s.prototype.accept(n, r) = new this.constructor(this.input, n, r, this.memo)],
 
-                                 _.memoize                             = caterwaul.memoize.from(fn[c, as, f][m[f.id] || (m[f.id] = f.apply(c, as)), where[m = as[0].memo]]),
-                                 _.identify(f)                         = f /se[_.id = caterwaul.gensym()],
-                                 _.parser(f)                           = _.identify(_.memoize(f)),
-                                 _.defparser(name, f)                  = _.parsers[name]() = _.parser(f.apply(this, arguments)),
-                                 _.parsers                             = {}]}).
+                                 _.memoize               = caterwaul.memoize.from(fn[c, as, f][k in m ? m[k] : (m[k] = f.apply(c, as)),
+                                                                                               where[k = '#{f.original.memo_id}|#{as[0].i}', m = as[0].memo || (as[0].memo = {})]]),
+                                 _.promote_non_states(f) = fn[state][f.call(this, state.constructor === _.parse_state ? state : _.parse_state.from_input(state))],
+                                 _.identify(f)           = f /se[_.memo_id = caterwaul.gensym()],
+                                 _.parser(f)             = _.promote_non_states(_.memoize(_.identify(f))),
+                                 _.defparser(name, f)    = _.parsers[name]() = _.parser(f.apply(this, arguments)),
+                                 _.parsers               = {}]}).
 
 // Notation.
 // Parsers are written as collections of named nonterminals. Each nonterminal contains a mandatory expansion and an optional binding:
@@ -47,25 +46,26 @@
 //   | peg[c('x')]         // Parses the string 'x'
 //     peg[c('foo bar')]   // Parses the string 'foo bar'
 
-  tconfiguration('std seq continuation', 'parser.c', function () {
-    this.configure('parser.core').parser.defparser('c', fn[x][fn[state][state.accept(x.length, x), when[x === state.input.substring(state.i, x.length)]]])}).
+    tconfiguration('std seq continuation', 'parser.c', function () {
+      this.configure('parser.core').parser.defparser('c', fn[x][fn[state][state.accept(state.i + x.length, x), when[x === state.input.substr(state.i, x.length)]]])}).
 
 //   Sequences.
 //   Denoted using the '%' operator. The resulting AST is flattened into a finite caterwaul sequence. For example:
 
 //   | peg[c('a') % c('b') % c('c')]('abc')                     // -> ['a', 'b', 'c']
-//     peg[c('a') % c('b')] >>= fn[xs][xs.join('/')]('ab')      // -> 'a/b'
+//     peg[c('a') % c('b') >>= fn[xs][xs.join('/')]]('ab')      // -> 'a/b'
 
-  tconfiguration('std seq continuation', 'parser.seq', function () {
-    this.configure('parser.core').parser.defparser('seq', fn_[l[as = seq[~arguments]] in fn[state][as.foldl(fn[st, f][st && st.then(f(st))], state)]])}).
+    tconfiguration('std opt seq continuation', 'parser.seq', function () {
+      this.configure('parser.core').parser.defparser('seq', fn_[l[as = arguments] in fn[state][
+        call/cc[fn[cc][opt.unroll[i, as.length][(state = as[i](state)) ? result.push(state.result) : cc(false)], state.accept(state.i, result)]], where[result = []]]])}).
 
 //   Alternatives.
 //   Denoted using the '/' operator. Alternation is transparent; that is, the chosen entry is returned identically. Entries are tried from left to right without backtracking. For example:
 
 //   | peg[c('a') / c('b')]('a')                                // -> 'a'
 
-  tconfiguration('std seq continuation', 'parser.alt', function () {
-    this.configure('parser.core').parser.defparser('alt', fn_[l[as = seq[~arguments]] in fn[state][as.exists(fn[f][f(state)])]])}).
+    tconfiguration('std seq', 'parser.alt', function () {
+      this.configure('parser.core').parser.defparser('alt', fn_[l[as = seq[~arguments]] in fn[state][seq[as |[r = _(state)]] && r, where[r = null]]])}).
 
 //   Repetition.
 //   Denoted using subscripted ranges, similar to the notation used in regular expressions. For example:
@@ -73,19 +73,20 @@
 //   | peg[c('a')[0]]                   // Zero or more 'a's
 //     peg[c('b')[1,4]                  // Between 1 and 4 'b's
 
-  tconfiguration('std seq continuation', 'parser.times', function () {
-    this.configure('parser.core').parser.defparser('times', fn[p, lower, upper][fn[state][
-      upper -= lower,
-      call/cc[fn[cc][opt.unroll[i, lower][(state = p(state)) ? result.push(state.result) : cc(false)], true]] &&
-      call/cc[l*[count = 0, loop(cc) = ++count < upper ? (state = p(state)) ? result.push(state.result) && call/tail[loop(cc)] : cc(false) : cc(state.accept(0, result))] in loop]]])}).
+    tconfiguration('std opt seq continuation', 'parser.times', function () {
+      this.configure('parser.core').parser.defparser('times', fn[p, lower, upper][fn[state][
+        upper && (upper -= lower),
+        call/cc[fn[cc][opt.unroll[i, lower][(state = p(state)) ? result.push(state.result) : cc(false)], true]] &&
+        call/cc[l*[count = 0, loop(cc) = (! upper || ++count < upper) && (state = p(state)) ? result.push(state.result) && call/tail[loop(cc)] : cc(state.accept(state.i, result))] in loop],
+        where[result = []]]])}).
 
 //   Optional things.
 //   Denoted using arrays. Returns a tree of undefined if the option fails to match. For example:
 
 //   | peg[c('a') % [c('b')] % c('c')]  // a followed by optional b followed by c
 
-  tconfiguration('std seq continuation', 'parser.opt', function () {
-    this.configure('parser.core').parser.defparser('opt', fn[p][fn[state][state.accept(n, r), where*[s = p(state), n = s && s.i - state.i, r = s && s.result]]])}).
+    tconfiguration('std seq continuation', 'parser.opt', function () {
+      this.configure('parser.core').parser.defparser('opt', fn[p][fn[state][state.accept(n, r), where*[s = p(state), n = s ? s.i : state.i, r = s && s.result]]])}).
 
 //   Positive and negative matches.
 //   Denoted using unary + and -, respectively. These consume no input but make assertions:
@@ -93,15 +94,13 @@
 //   | peg[c('a') % +c('b')]            // Matches an 'a' followed by a 'b', but consumes only the 'a'
 //     peg[c('a') % -c('b')]            // Matches an 'a' followed by anything except 'b', but consumes only the 'a'
 
-  tconfiguration('std seq continuation', 'parser.match', function () {
-    this.configure('parser.core').parser /se[_.defparser('match',  fn[p][fn[state][p(state) /re[_  && state.accept(0, _.result)]]]),
-                                             _.defparser('reject', fn[p][fn[state][p(state) /re[!_ && state.accept(0, null)]]])]}).
+    tconfiguration('std seq continuation', 'parser.match', function () {
+      this.configure('parser.core').parser /se[_.defparser('match',  fn[p][fn[state][p(state) /re[_  && state.accept(0, _.result)]]]),
+                                               _.defparser('reject', fn[p][fn[state][p(state) /re[!_ && state.accept(0, null)]]])]}).
 
-//   Lazy matching.
-//   Because all peg[] expressions eta-expand the functions they generate, you can refer to rules that don't exist at definition-time:
+// Final configuration.
+// Loads both the classes and the peg[] macro.
 
-//   | parse('xyz'),
-//     where*[rule1 = peg[c('x') / rule2 >>= fn_['x']],
-//            rule2 = peg[c('X')],
-//            parse = peg[rule1 % c('y') % c('z')]];
+  configuration('parser', function () {
+    this.configure('parser.core parser.c parser.seq parser.alt parser.times parser.opt parser.match')});
 // Generated by SDoc 
