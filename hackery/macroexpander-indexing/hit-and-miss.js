4849d05e89c3499a12824db70f43481e77d73749
Spencer Tipping <spencer@spencertipping.com>
2011-03-30 14:21:09 -0600

    Comparing between long and short keys. Makes very little difference on SpiderMonkey.

--- hackery/macroexpander-indexing/hit-and-miss.js
+++ hackery/macroexpander-indexing/hit-and-miss.js
@@ -5,15 +5,20 @@ var total  = 0;
 var count  = 100000000;
 
 for (var i = 0; i < 1000; ++i)
-  object['prop' + i] = true;
+  object['property' + i] = object['p' + i] = true;
 
 var iterations = 3000000;
 
 var l = typeof console === 'undefined' ? print : function (x) {console.log(x)};
 
-l('100% hit ratio: ' + (bench(iterations, function () {if (object['prop' + ++count % 1000]) ++total})));
-l('50% hit ratio:  ' + (bench(iterations, function () {if (object['prop' + ++count % 2000]) ++total})));
-l('25% hit ratio:  ' + (bench(iterations, function () {if (object['prop' + ++count % 4000]) ++total})));
-l('0% hit ratio:   ' + (bench(iterations, function () {if (object['proj' + ++count % 1000]) ++total})));
+l('100% hit ratio (long):  ' + (bench(iterations, function () {if (object['property' + ++count % 1000]) ++total})));
+l('50% hit ratio (long):   ' + (bench(iterations, function () {if (object['property' + ++count % 2000]) ++total})));
+l('25% hit ratio (long):   ' + (bench(iterations, function () {if (object['property' + ++count % 4000]) ++total})));
+l('0% hit ratio (long):    ' + (bench(iterations, function () {if (object['projerty' + ++count % 1000]) ++total})));
+
+l('100% hit ratio (short): ' + (bench(iterations, function () {if (object['p' + ++count % 1000]) ++total})));
+l('50% hit ratio (short):  ' + (bench(iterations, function () {if (object['p' + ++count % 2000]) ++total})));
+l('25% hit ratio (short):  ' + (bench(iterations, function () {if (object['p' + ++count % 4000]) ++total})));
+l('0% hit ratio (short):   ' + (bench(iterations, function () {if (object['j' + ++count % 1000]) ++total})));
 
 l(total);
