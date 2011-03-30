8f8f6f058d0e08534d38feb63c29f1586ed85790
Spencer Tipping <spencer@spencertipping.com>
2011-03-30 12:25:34 -0600

    More benchmarks, including <= for integers and strings. It may be more feasible to implement a SpiderMonkey hashtable than to use their data structures.

--- hackery/macroexpander-indexing/string-compare-vs-hash.js
+++ hackery/macroexpander-indexing/string-compare-vs-hash.js
@@ -4,13 +4,25 @@ var object = {};
 var total  = 0;
 var count  = 100000000;
 
-var log    = typeof console === 'undefined' ? print : function (x) {console.log(x)};
+var iterations = 3000000;
 
-log('index with integers: ' + bench(10000000, function () {if (object[++count]) ++total}));
-log('index with strings:  ' + bench(10000000, function () {if (object[(++count).toString()]) ++total}));
-log('integer compare ===: ' + bench(10000000, function () {if (count === ++count) ++total}));
-log('string compare ===:  ' + bench(10000000, function () {if (count.toString() === (++count).toString()) ++total}));
-log('integer compare ==:  ' + bench(10000000, function () {if (count == ++count) ++total}));
-log('string compare ==:   ' + bench(10000000, function () {if (count.toString() == (++count).toString()) ++total}));
+var to_i   = bench(iterations, function () {total += ++count});
+var to_s   = bench(iterations, function () {total += (++count).toString().length});
 
-log(total);
+var l      = typeof console === 'undefined' ? print : function (x) {console.log(x)};
+
+l('to_s adjustment:           ' + to_s);
+l('to_i adjustment:           ' + to_i);
+
+l('index with integers: (adj) ' + (bench(iterations, function () {if (object[++count]) ++total}) - to_i));
+l('index with strings: (adj)  ' + (bench(iterations, function () {if (object[(++count).toString()]) ++total}) - to_s));
+l('index with integers:       ' + (bench(iterations, function () {if (object[++count]) ++total}) - 0));
+l('index with strings:        ' + (bench(iterations, function () {if (object[(++count).toString()]) ++total}) - 0));
+l('integer compare ===:       ' + (bench(iterations, function () {if (count === ++count) ++total}) - 0));
+l('string compare ===:        ' + (bench(iterations, function () {if (count.toString() === (++count).toString()) ++total}) - 0 * 2));
+l('integer compare <=:        ' + (bench(iterations, function () {if (count <= ++count) ++total}) - 0));
+l('string compare <=:         ' + (bench(iterations, function () {if (count.toString() <= (++count).toString()) ++total}) - 0 * 2));
+l('integer compare ==:        ' + (bench(iterations, function () {if (count == ++count) ++total}) - 0));
+l('string compare ==:         ' + (bench(iterations, function () {if (count.toString() == (++count).toString()) ++total}) - 0 * 2));
+
+l(total);
