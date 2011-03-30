a1b5177f1f70d0943662fa60de65eb01d4c4a5a2
Spencer Tipping <spencer@spencertipping.com>
2011-03-30 10:53:32 -0600

    Logging cumulative icount with cumulative complexity

--- hackery/coarse-profiling/caterwaul.profiled.js
+++ hackery/coarse-profiling/caterwaul.profiled.js
@@ -6,22 +6,13 @@ caterwaul.tconfiguration('std', 'profile', function () {
 
   this.method('macroexpand', profile('macroexpand', this.macroexpand)),
 
-  setTimeout(fn_[log(icount(caterwaul))], 0),
-
   where*[log_function                = typeof console !== 'undefined' ? console/mb/log : print,
          log(x)                      = log_function(x) /re[x],
-         recursion                   = 0,
-         last_icount                 = 0,
-         icounts                     = [],
-         enter(name, c)              = '(#{icount_delta(c)} / #{icount(c) - icounts[icounts.length - 1] || 0})    ' /se[++recursion, icounts.push(icount(c))],
-         exit(name, c)               = '(#{icount_delta(c)} / #{icount(c) - icounts.pop()})    ' /se[--recursion],
-
-         spaces(n)                   = n ? ' ' + spaces(n - 1) : '',
-
-         icount_delta(c)             = -(last_icount - icount(c) /se[last_icount = _]),
          icount(c)                   = c.invocation_count && c.invocation_count(),
+
+         total_icount                = 0,
+         total_complexity            = 0,
 
          complexity_of(tree)         = l[total = 1] in tree.reach(fn_[++total]) /re[total],
 
-         profile(name, f)(t)         = log('#{spaces(recursion)}#{name} #{enter(name, this)}: #{complexity_of(t)}: #{t.serialize()}') &&
-                                       f.apply(this, arguments) /se[log('#{spaces(recursion - 1)}#{name} #{exit(name, this)}: #{complexity_of(_)}: #{_.serialize()}')]]});
+         profile(name, f)(t)         = f.apply(this, arguments) /se[log('#{total_icount += icount(this)} #{total_complexity += complexity_of(t)}')]]});
