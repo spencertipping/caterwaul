841ab0e0506fabe4b52d21351c354e6475519af7
Spencer Tipping <spencer@spencertipping.com>
2011-03-30 11:28:28 -0600

    Thinking about ways to optimize macroexpansion, and built lists of all of the macros that caterwaul ends up creating.

--- hackery/coarse-profiling/caterwaul.profiled-core.js
+++ hackery/coarse-profiling/caterwaul.profiled-core.js
@@ -1279,17 +1279,21 @@ caterwaul.tconfiguration('std', 'profile', function () {
 
   this.method('macroexpand', profile('macroexpand', this.macroexpand)),
 
+  setTimeout(fn_[log(total_complexity)], 0),
+
   where*[log_function                = typeof console !== 'undefined' ? console/mb/log : print,
          log(x)                      = log_function(x) /re[x],
          icount(c)                   = c.invocation_count && c.invocation_count(),
 
+         total_complexity            = 0,
+
          icount_stack                = [],
 
          enter(c, t)                 = icount_stack.push(icount(c)),
-         exit(c, t)                  = log('#{complexity_of(t)} #{(icount(c) - icount_stack.pop()) / c.macro_patterns.length}'),
+         exit(c, t)                  = log('#{complexity_of(t)} #{(icount(c) - icount_stack.pop()) / c.macro_patterns.length}')
+                                       /se[icount_stack.length || (total_complexity += complexity_of(t))],
 
          complexity_of(tree)         = l[total = 1] in tree.reach(fn_[++total]) /re[total],
-
          profile(name, f)(t)         = enter(this, t) && f.apply(this, arguments) /se[exit(this, t)]]});
 caterwaul.configure('profile');
 // Caterwaul optimization library | Spencer Tipping
