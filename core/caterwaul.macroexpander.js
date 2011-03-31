973c84bf2101bfc0b65f517bbf681b7774e90560
Spencer Tipping <spencer@spencertipping.com>
2011-03-31 13:21:13 -0600

    Working on the hashed macroexpander. This should provide significant optimization benefits over the naive macroexpander.

--- core/caterwaul.macroexpander.js
+++ core/caterwaul.macroexpander.js
@@ -15,9 +15,7 @@
 //   });
 
 // Wrapping the first function in caterwaul() wasn't necessary, though it was helpful to get the qs[] and fn[] shorthands. In this case, the macro is persistent to the caterwaul function that it
-// was called on. (So any future caterwaul()ed functions would have access to it.)
-
-// You can also define conditional macros, though they will probably be slower. For example:
+// was called on. (So any future caterwaul()ed functions would have access to it.) You can also define conditional macros, though they will probably be slower. For example:
 
 // | caterwaul(function () {
 //     caterwaul.rmacro(qs[let (_) in _], fn[bs, e][bs.data === '=' && ...]);
@@ -37,12 +35,11 @@
 // | caterwaul(function () {
 //     var patterns = [qs[foo], qs[bar], qs[bif]];
 //     patterns.map (function (p) {
-//       caterwaul.rmacro (p, fn[x][...]);
+//       caterwaul.rmacro(p, fn[x][...]);
 //     });
 //   }) ();
 
-// This gives Caterwaul the opportunity to call your function only on relevant nodes. (Note that at present I haven't found an algorithm to make things any faster than using a depth-first scan.
-// However, if I do find such an algorithm later on then macroexpansion will run quite a bit faster for programs with well-defined patterns.)
+// Caterwaul implements several optimizations that make it much faster to macroexpand code when the macro patterns are easily identified.
 
 //   Pitfalls of macroexpansion.
 //   Macroexpansion as described here can encode a lambda-calculus. The whole point of having macros is to make them capable, so I can't complain about that. But there are limits to how far I'm
@@ -67,40 +64,9 @@
 
 //   Things that are not guaranteed:
 
-//   | 1. Reassembly of different pieces (see above)
+//   | 1. Reassembly of different pieces (see above).
 //     2. Anything at all, if you modify the syntax tree in the macro code. Returning a replacement is one thing, but modifying one will break things.
 //     3. Performance bounds.
 
-//   Matching.
-//   macro_try_match returns null if two syntax trees don't match, or a possibly empty array of wildcards if the given tree matches the pattern. Wildcards are indicated by '_' nodes, as
-//   illustrated in the macro definition examples earlier in this section. Note that this function is O(n) in the number of nodes in the pattern. It is optimized, though, to reject invalid nodes
-//   quickly -- that is, if there is any mismatch in arity or data.
-
-  var macro_array_push = Array.prototype.push,
-      macro_try_match  = function (pattern, t) {if (pattern.data === '_')                                   return [t];
-                                                if (pattern.data !== t.data || pattern.length !== t.length) return null;
-                                                for (var i = 0, l = pattern.length, wildcards = [], match = null; i < l; ++i)
-                                                  if (match = macro_try_match(pattern[i], t[i])) macro_array_push.apply(wildcards, match);
-                                                  else                                           return null;
-                                                return wildcards},
-
-//   Expansion.
-//   Uses the straightforward brute-force algorithm to go through the source tree and expand macros. At first I tried to use indexes, but found that I couldn't think of a particularly good way to
-//   avoid double-expansion -- that is, problems like qs[qs[foo]] -- the outer must be expanded without the inner one. Most indexing strategies would not reliably (or if reliably, not profitably)
-//   index the tree in such a way as to encode containment. Perhaps at some point I'll find a faster macroexpander, especially if this one proves to be slow. At this point macroexpansion is by
-//   far the most complex part of this system, at O(nki) where n is the number of parse tree nodes, k is the number of macros, and i is the number of nodes in the macro pattern tree. (Though in
-//   practice it's generally not quite so bad.)
-//   
-//   Note! This function by default does not re-macroexpand the output of macros. That is handled at a higher level by Caterwaul's macro definition facility (see the 'rmacro' method).
-
-//   The fourth parameter, 'context', is used to hand a 'this' reference to the macroexpander. This is necessary to get defmacro[] to work properly, and in general lets macros be side-effectful.
-//   (Not that you should be in the habit of defining side-effectful macros, but I certainly won't stop you.)
-
-//   Note that as of version 0.5, macroexpansion proceeds backwards. This means that the /last/ matching macro is used, not the first. It's an important feature, as it lets you write new macros
-//   to override previous definitions. This ultimately lets you define sub-caterwaul functions for DSLs, and each can define a default case by matching on qs[_] (thus preventing access to other
-//   macro definitions that may exist).
-
-    macro_expand = function (t, macros, expanders, context) {
-                     return t.rmap(function (n) {for (var i = macros.length - 1, macro, match, replacement; i >= 0 && (macro = macros[i]); --i)
-                                                   if ((match = macro_try_match(macro, n)) && (replacement = expanders[i].apply(context, match))) return replacement})};
+- pinclude pp::js::core/caterwaul.macroexpander.naive
 // Generated by SDoc 
