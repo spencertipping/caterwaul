6b0f4c400dcfe52e7db85425311b335b0d1d1d56
Spencer Tipping <spencer@spencertipping.com>
2011-08-08 21:58:50 -0600

    Deleted the boring phone book example and replaced it with a very epic quicksort app that is presently vaporware.

--- doc/examples/phone-book/overloaded-val.js
+++
@@ -1,5 +0,0 @@
-caterwaul.js_all()(function () {
-  var original = $.fn.val;
-  $.fn.overload_val(f) = this.data('overloaded-val', f);
-  $.fn.val() = (this.data('overloaded-val') || original).apply(this, arguments);
-})();
