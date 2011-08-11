ee032691826058927874eb495170e52c3f4912e8
Spencer Tipping <spencer@spencertipping.com>
2011-08-11 07:48:38 -0600

    Update examples to reflect new caterwaul naming

--- doc/examples/primes/primes.js
+++ doc/examples/primes/primes.js
@@ -2,5 +2,5 @@ $(caterwaul.js_all()(function () {
   $('body').append(primes.join(', '))
   -where [
     composite(n) = n[2, Math.sqrt(n) + 1] |[n % x === 0] |seq,
-    primes       = n[2, 10000] %[! composite(x)] -seq];
+    primes       = n[2, 10000] %!composite -seq];
 }));
