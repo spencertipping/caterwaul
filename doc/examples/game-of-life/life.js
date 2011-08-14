4a4b670cc0a023b72d5b5d722725c3a5e3ecffa2
Spencer Tipping <spencer@spencertipping.com>
2011-08-13 22:03:12 -0600

    Added a game of life example; now just need to explain the code.

---
+++ doc/examples/game-of-life/life.js
@@ -0,0 +1,44 @@
+$(caterwaul.jquery(caterwaul.js_all())(function () {
+  $('body').append(top_button_row, board.div)
+  -where [
+    board           = life_board(30, 30),
+    interval        = null,
+    run_or_stop()   = interval ? stop_running()  -se- $(this).text('Run') :
+                                 start_running() -se- $(this).text('Stop'),
+    start_running() = interval = setInterval(board.step, 100),
+    stop_running()  = clearInterval(interval) -se [interval = null],
+    top_button_row  = jquery in div(
+                        button.step('Step') /click(board.step),
+                        button.run('Run')   /click(run_or_stop))]
+  -where [
+    life_board(x, y)    = {cells: cells,
+                           div:   div_for(cells),
+                           step:  step_function(cells)}
+                          -where [cells = cells_for(x, y)],
+
+    cells_for(x, y)     = n[x] *~[n[y] *y[cell_for(x, y)]] -seq,
+    cell_for(x, y)      = jquery in div.cell *!x(x) *!y(y) %position(x, y)
+                                                           %invert_on_click,
+    position(x, y)(e)   = e.css({position: 'absolute',
+                                 left: x * 12, width:  10,
+                                 top:  y * 12, height: 10}),
+
+    invert_on_click(e)  = e.mousedown($(this).toggleClass('on') -given.e),
+    div_for(cs)         = jquery [div.board]
+                          -se- cs *!~[x *![it.append(x)]] /seq,
+    step_function(cs)() =
+      cs *!~[x *!update] -seq
+      -where [
+        new_state(x, y) = on(x, y) ? count(x, y) -re [it >= 2 && it <= 3] :
+                                     count(x, y) === 3,
+        count(x, y)     = adjacent(x, y) /[x + x0] -seq,
+        adjacent(x, y)  = ni[x - 1, x + 1] *~!~xn[ni[y - 1, y + 1]
+                            %yn[x !== xn || y !== yn]
+                            *yn[+on(xn, yn)]] -seq,
+        cell(x, y)      = cs[wrap(x, cs)] -re- it[wrap(y, it)],
+        wrap(x, xs)     = (x + xs.length) % xs.length,
+        on(x, y)        = cell(x, y).hasClass('on'),
+
+        new_states      = cs *~[x *y[new_state(xi, yi)]] -seq,
+        update(cell)    = cell.toggleClass('on',
+                            new_states[cell.data('x')][cell.data('y')])]]}));
