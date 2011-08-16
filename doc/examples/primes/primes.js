63aa18d8fec08abd6d8c6b94bf6d2f5621bfcf29
Spencer Tipping <spencer@spencertipping.com>
2011-08-16 07:00:10 -0600

    Minor edits

--- doc/examples/primes/primes.js
+++ doc/examples/primes/primes.js
@@ -1,5 +1,5 @@
 $(caterwaul.js_all()(function () {
-  $('body').append(primes.join(', '))
+  $('body').text(primes.join(', '))
   -where [
     composite(n) = ni[2, Math.sqrt(n)] |[n % x === 0] |seq,
     primes       = n[2, 10000] %!composite -seq];
