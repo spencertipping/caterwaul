b1a74b2b2095f7018e5098e7fb1470e139104c58
Spencer Tipping <spencer@spencertipping.com>
2012-01-08 09:25:08 -0700

    Bootstrapping. This allows calls to parse() to be replaced by cheaper direct syntax constructor calls, albeit at the cost of larger code.

--- build/std.js-literals.js
+++ build/std.js-literals.js
@@ -1,6 +1,6 @@
-caterwaul.module( 'std.js-literals' ,function($) {$.js_literals=function(caterwaul_function) {var function_template=$.parse( 'function (_) {return _body}' ) ;
+caterwaul.module( 'std.js-literals' , (function(qs_h_O6cwvNBuCkvZGt6SXLaAyY,qs_i_O6cwvNBuCkvZGt6SXLaAyY) {var result_j_O6cwvNBuCkvZGt6SXLaAyY= (function($) {$.js_literals=function(caterwaul_function) {var function_template=qs_h_O6cwvNBuCkvZGt6SXLaAyY;
  (function(r) {r.x=$.reexpander(function(node) {return node.with_data(node.data.replace( /\s+/g , '' ) ) } ) ;
-var call_exec_template=$.parse( '_regexp.exec(_)' ) ;
+var call_exec_template=qs_i_O6cwvNBuCkvZGt6SXLaAyY;
 r.qf=function(node) {return function_template.replace( {_body:call_exec_template.replace( {_regexp:node} ) } ) } } ) (caterwaul_function.literal_modifiers.regexp) ;
  (function(s) {s.qw=$.reexpander(function(node) {for(var array_node=new $.syntax( '[' ) ,comma=new $.syntax( ',' ) ,delimiter=node.data.charAt(0) ,pieces=node.as_escaped_string() .split( /\s+/ ) ,i=0,l=pieces.length;
 i<l;
@@ -14,3 +14,5 @@ s.qr=$.reexpander(function(node) {return node.with_data( '/' +node.as_escaped_st
 s.qs=function(node) {return new $.expression_ref($.syntax_to_expression($.parse(node.as_unescaped_string() ) ) , 'qs' ) } ;
 s.qf=$.reexpander(function(node) {return function_template.replace( {_body:$.parse(node.as_unescaped_string() ) } ) } ) } ) (caterwaul_function.literal_modifiers.string) ;
 return caterwaul_function} } ) ;
+result_j_O6cwvNBuCkvZGt6SXLaAyY.caterwaul_expression_ref_table= { "qs_h_O6cwvNBuCkvZGt6SXLaAyY" : ( "new caterwaul.syntax( \"function\" ,new caterwaul.syntax( \"(\" ,new caterwaul.syntax( \"_\" ) ) ,new caterwaul.syntax( \"{\" ,new caterwaul.syntax( \"return\" ,new caterwaul.syntax( \"_body\" ) ) ) )" ) , "qs_i_O6cwvNBuCkvZGt6SXLaAyY" : ( "new caterwaul.syntax( \"()\" ,new caterwaul.syntax( \".\" ,new caterwaul.syntax( \"_regexp\" ) ,new caterwaul.syntax( \"exec\" ) ) ,new caterwaul.syntax( \"_\" ) )" ) } ;
+return(result_j_O6cwvNBuCkvZGt6SXLaAyY) } ) .call(this,new caterwaul.syntax( "function" ,new caterwaul.syntax( "(" ,new caterwaul.syntax( "_" ) ) ,new caterwaul.syntax( "{" ,new caterwaul.syntax( "return" ,new caterwaul.syntax( "_body" ) ) ) ) ,new caterwaul.syntax( "()" ,new caterwaul.syntax( "." ,new caterwaul.syntax( "_regexp" ) ,new caterwaul.syntax( "exec" ) ) ,new caterwaul.syntax( "_" ) ) ) ) ;
