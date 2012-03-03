3321a634223154791a0a92422072112eb6bc25dc
Spencer Tipping <spencer@spencertipping.com>
2012-03-03 08:00:16 -0700

    Right now the seq library fails to load; trying to figure out why.

--- build/std.anonymize.js
+++ build/std.anonymize.js
@@ -1,4 +1,14 @@
-caterwaul.module( 'std.anon' ,function($) {$.anonymizer=function( ) {for(var translation_table= { } ,i=0,l=arguments.length;
+caterwaul.module( 'std.anon' ,function($) {$.anonymizer=function() {var xs=arguments;
+return(function( ) {var table= (function(o) {for(var r= { } ,i=0,l=o.length,x;
 i<l;
- ++i)translation_table[arguments[i] ] =$.gensym(arguments[i] ) ;
-return function(node) {return $.parse(node) .replace(translation_table) } } } ) ;
+ ++i)x=o[i] ,r[x[0] ] =x[1] ;
+return r} ) .call(this, ( (function(xs) {var x,x0,xi,xl,xr;
+for(var xr=new xs.constructor() ,xi=0,xl=xs.length;
+xi<xl;
+ ++xi)x=xs[xi] ,xr.push( ( [x,$.gensym(x) ] ) ) ;
+return xr} ) .call(this, (function(xs) {var x,x0,xi,xl,xr;
+for(var xr=new xs.constructor() ,xi=0,xl=xs.length;
+xi<xl;
+ ++xi)x=xs[xi] ,xr.push.apply(xr,Array.prototype.slice.call( (x.constructor===Array?x:x.split( ' ' ) ) ) ) ;
+return xr} ) .call(this,xs) ) ) ) ;
+return(function(_) {return( ($) .parse(_) ) .replace(table) } ) } ) .call(this) } } ) ;
